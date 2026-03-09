85c0d18bc97d0ed0d8fd0610987923720f085757
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-09 16:19:07 +0530

    Add Windows runner files for high DPI support and console integration
    - Created runner.exe.manifest to enable DPI awareness for the application.
    - Implemented utils.cpp and utils.h for console creation and command line argument handling.
    - Developed win32_window.cpp and win32_window.h to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ random_quota_generator_app4/android/app/src/main/kotlin/com/example/random_quota_generator_app4/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.random_quota_generator_app4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ random_quota_generator_app4/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(RandomQuota());
+}
+
+class RandomQuota extends StatelessWidget{
+  const RandomQuota({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:"Random Quota App",
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
