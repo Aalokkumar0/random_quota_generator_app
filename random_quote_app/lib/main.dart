394fd6ccd9ae57ca64e439f024fba9f89d0ef2e3
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-23 12:40:49 +0530

    adding style

--- random_quote_app/lib/main.dart
+++ random_quote_app/lib/main.dart
@@ -4,19 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       title: "Random Quote ",
-      home:Scaffold(
-      appBar: AppBar(
-        title: Text("Read random quote", style: TextStyle(color: Colors.amber)),
-      ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.green,
-        child: Center(
-          
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Read random quote",
+            style: TextStyle(color: Colors.amber),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            height: 400,
+            width: 400,
+            color: Colors.black,
+            child: Center(
+              child: Text(
+                "HELLO, AALOK ",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 34,
+                  fontWeight: FontWeight.w900,
+                  
+                ),
+              ),
+            ),
+          ),
         ),
       ),
-      )
     ),
   );
 }
