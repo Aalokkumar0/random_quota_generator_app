d17dbc66b3fc24230c489bccbd07b1cb842ebc89
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-23 12:45:59 +0530

    banner

--- random_quote_app/lib/main.dart
+++ random_quote_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Random Quote ",
       home: Scaffold(
         appBar: AppBar(
